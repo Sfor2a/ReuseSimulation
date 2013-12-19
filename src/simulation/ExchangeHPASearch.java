5c566d6359e2812ef417a0a11af425024f4369d9
Sfor2a <sforza_aria.vip-skyline@aqua.plala.or.jp>
2013-12-19 18:33:50 +0900

    地図の追加
    MAXValueの再修正
    東京の地下鉄をこうさてんのモデルにする地図をついか

--- src/simulation/ExchangeHPASearch.java
+++ src/simulation/ExchangeHPASearch.java
@@ -5,7 +5,7 @@ import housedata.HPAdata;
 import housedata.Housedata;
 
 public class ExchangeHPASearch {
-	private int Cost = ( Integer.MAX_VALUE ) / 4;
+	private int Cost = ( Integer.MAX_VALUE ) / 1000;
 	private HPAdata HPA1;
 	
 	public ExchangeHPASearch ( Housedata A1, Housedata A2 ) {
