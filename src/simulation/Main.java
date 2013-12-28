83d3400fa6949d5918c414b10bb32d09ba46b3f5
Sfor2a <sforza_aria.vip-skyline@aqua.plala.or.jp>
2013-12-28 15:46:29 +0900

    最新にリセット

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		CostAndRangeRanking CRR = new CostAndRangeRanking ();
 		
-		for ( int i = 0; i<110; i++ ) {
+		for ( int i = 0; i<25; i++ ) {
 			System.out.println(i+"ターン目");
 			CRR.CARRCreate(i); //ランクを作り、交換し
 			WriteOutData WOR = new WriteOutData();
